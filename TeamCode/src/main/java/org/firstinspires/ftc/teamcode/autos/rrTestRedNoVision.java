66bc31ca68aba2c0def02aa602455cca5df2c655
Jonathan Chen <trappedc072506@gmail.com>
2024-03-12 21:47:49 -0400

    changed some values, done red front

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/rrTestRedNoVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/rrTestRedNoVision.java
@@ -28,30 +28,26 @@ public class rrTestRedNoVision extends LinearOpMode{
 
         if (isStopRequested()) return;
 
-        TrajectorySequence frontL = drive.trajectorySequenceBuilder(new Pose2d(-35, -61.5, Math.toRadians(90)))
-                .lineTo(new Vector2d(-47,-52))
-                .waitSeconds(5)
-                .addTemporalMarker(0,()-> {
-                    encoded.armtoGround();
-                    encoded.closeClaw();})
-                .addTemporalMarker(1,()-> {encoded.openBottomClaw();})
-                .addTemporalMarker(3,()-> {encoded.armScoreAuto();})
+        TrajectorySequence frontM = drive.trajectorySequenceBuilder(startPose)
+                .addTemporalMarker(0,()->{encoded.closeClaw();})
+                .waitSeconds(2)
+                .lineToSplineHeading(new Pose2d(-36.5,-44.5,Math.toRadians(45)))
+                .waitSeconds(2)
+                .addTemporalMarker(1,()-> {
+                    encoded.armtoGroundAuto();})
+                .addTemporalMarker(3,()-> {encoded.openBottomClaw();})
+                .addTemporalMarker(4,()-> {encoded.armScoreAuto();})
                 //purple dropped
-                .lineToSplineHeading(new Pose2d(-35,-59,Math.toRadians(0)))
-                .waitSeconds(1)
+                .lineToSplineHeading(new Pose2d(-36,-59,Math.toRadians(0)))
+                .waitSeconds(0.5)
                 .splineTo(new Vector2d(10,-59),Math.toRadians(0))
-                .splineTo(new Vector2d(49,-31),Math.toRadians(0))
-                .waitSeconds(1)
-                .addTemporalMarker(13,()-> {encoded.openTopClaw();})
+                .splineTo(new Vector2d(54,-42),Math.toRadians(0))
+                .waitSeconds(2)
+                .addTemporalMarker(11,()-> {encoded.openTopClaw();})
                 //yellow dropped
-                .setReversed(true)
-                .splineTo(new Vector2d(10,-58),Math.toRadians(180))
-                .splineTo(new Vector2d(-35,-58),Math.toRadians(180))
-                .setReversed(false)
-                .lineToSplineHeading(new Pose2d(-55,-47,Math.toRadians(135)))
-                .addTemporalMarker(14,()-> {encoded.openTopClaw();})
+                .lineTo(new Vector2d(50,-60))
                 .build();
-        drive.followTrajectorySequence(frontL);
+        drive.followTrajectorySequence(frontM);
 
         }
     }
