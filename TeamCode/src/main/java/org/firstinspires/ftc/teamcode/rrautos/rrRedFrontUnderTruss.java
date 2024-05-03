d67501f6d6cd4748bd4934885f52bc8a5620222e
koolfyn <itsang1004@gmail.com>
2024-05-03 14:42:02 -0400

    code robojawn ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrautos/rrRedFrontUnderTruss.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrautos/rrRedFrontUnderTruss.java
@@ -55,7 +55,7 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                 .splineTo(new Vector2d(53,-29),Math.toRadians(0))
                 .waitSeconds(1)
                 .addTemporalMarker(8.5,()-> {encoded.openTopClaw();})
-                .lineTo(new Vector2d(50,-58.5))
+                .lineTo(new Vector2d(48,-58.5))
                 //yellow dropped
                 //next few line is cycle to pixel stack not done yet
                 .setReversed(true)
@@ -64,11 +64,11 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                 .setReversed(false)
                 .lineToSplineHeading(new Pose2d(-49,-50,Math.toRadians(140)))
                 .forward(5)
-                .waitSeconds(1)
+                .waitSeconds(0.75 )
                 //at pixel stack
-                .addTemporalMarker(14,()-> {encoded.armtoPixelStack();})
-                .addTemporalMarker(15.5,()-> {encoded.closeClaw();})
-                .addTemporalMarker(16,()-> {encoded.armScoreAuto();})
+                .addTemporalMarker(13,()-> {encoded.armtoPixelStack();})
+                .addTemporalMarker(16,()-> {encoded.closeClaw();})
+                .addTemporalMarker(17,()-> {encoded.armScoreAuto();})
                 .lineToSplineHeading(new Pose2d(-36,-57,Math.toRadians(0)))
                 .waitSeconds(1)
                 .splineTo(new Vector2d(10,-57),Math.toRadians(0))
@@ -77,7 +77,7 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                 .addTemporalMarker(23,()-> {
                     encoded.openTopClaw();
                     encoded.openBottomClaw();})
-                .lineTo(new Vector2d(50,-58.5))
+                .lineTo(new Vector2d(48,-58.5))
                 .build();
         drive.followTrajectorySequence(frontL);
                 break;
@@ -96,11 +96,11 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                         .lineToSplineHeading(new Pose2d(-36,-59,Math.toRadians(0)))
                         .waitSeconds(0.5)
                         .splineTo(new Vector2d(10,-59),Math.toRadians(0))
-                        .splineTo(new Vector2d(54,-36),Math.toRadians(0))
+                        .splineTo(new Vector2d(54,-35),Math.toRadians(0))
                         .waitSeconds(1)
                         .addTemporalMarker(8.5,()-> {encoded.openTopClaw();})
                         //yellow dropped
-                        .lineTo(new Vector2d(50,-58.5))
+                        .lineTo(new Vector2d(48,-58.5))
                         .setReversed(true)
                         .splineTo(new Vector2d(10,-58),Math.toRadians(180))
                         .splineTo(new Vector2d(-35,-58),Math.toRadians(180))
@@ -120,7 +120,7 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                         .addTemporalMarker(23,()-> {
                             encoded.openTopClaw();
                             encoded.openBottomClaw();})
-                        .lineTo(new Vector2d(50,-58.5))
+                        .lineTo(new Vector2d(48,-58.5))
                         .build();
                 drive.followTrajectorySequence(frontM);
                 break;
@@ -130,18 +130,18 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                         .waitSeconds(1)
                         .addTemporalMarker(0,()->{encoded.closeClaw();})
                         .addTemporalMarker(1,()->{encoded.armtoGroundAuto();})
-                        .lineToSplineHeading(new Pose2d(-36,-48,Math.toRadians(45)))
+                        .lineToSplineHeading(new Pose2d(-35.5,-48,Math.toRadians(45)))
                         .waitSeconds(.1)
-                        .addTemporalMarker(2,()-> {encoded.openBottomClaw();})
-                        .addTemporalMarker(2.5,()-> {encoded.armScoreAuto();})
+                        .addTemporalMarker(1.7,()-> {encoded.openBottomClaw();})
+                        .addTemporalMarker(2,()-> {encoded.armScoreAuto();})
                         //purple dropped
                         .lineToSplineHeading(new Pose2d(-40,-59,Math.toRadians(0)))
                         .waitSeconds(.1)
                         .splineTo(new Vector2d(10,-59),Math.toRadians(0))
                         .splineTo(new Vector2d(54,-42),Math.toRadians(0))
                         .waitSeconds(1)
-                        .addTemporalMarker(8.5,()-> {encoded.openTopClaw();})
-                        .lineTo(new Vector2d(50,-58.5))
+                        .addTemporalMarker(7.5,()-> {encoded.openTopClaw();})
+                        .lineTo(new Vector2d(48,-58.5))
                         //yellow dropped
                         .setReversed(true)
                         .splineTo(new Vector2d(10,-58),Math.toRadians(180))
@@ -151,18 +151,18 @@ public class rrRedFrontUnderTruss extends LinearOpMode{
                         .forward(5)
                         .waitSeconds(1)
                         //at pixel stack
-                        .addTemporalMarker(14,()-> {encoded.armtoPixelStack();})
-                        .addTemporalMarker(15.5,()-> {encoded.closeClaw();})
-                        .addTemporalMarker(16  ,()-> {encoded.armScoreAuto();})
+                        .addTemporalMarker(12,()-> {encoded.armtoPixelStack();})
+                        .addTemporalMarker(14,()-> {encoded.closeClaw();})
+                        .addTemporalMarker(15,()-> {encoded.armScoreAuto();})
                         .lineToSplineHeading(new Pose2d(-36,-57,Math.toRadians(0)))
                         .waitSeconds(.5)
                         .splineTo(new Vector2d(10,-57),Math.toRadians(0))
                         .splineTo(new Vector2d(56,-34),Math.toRadians(0))
                         .waitSeconds(0.5)
-                        .addTemporalMarker(23,()-> {
+                        .addTemporalMarker(21,()-> {
                             encoded.openTopClaw();
                             encoded.openBottomClaw();})
-                        .lineTo(new Vector2d(50,-58.5))
+                        .lineTo(new Vector2d(48,-58.5))
                         .build();
                 drive.followTrajectorySequence(frontR);
 
